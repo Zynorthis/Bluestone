e2f2321e6e0c41c6eb8a2e1f22b5e9e30126c261
Jacob Taylor <jacobtaylor727@outlook.com>
2019-02-08 08:19:15 -0600

    Almost finished laying out the home screen, just need to re-do the spacing for the cards linking to the respective card or calendar before it will be 100% done for now.

--- bluestone/lib/main.dart
+++ bluestone/lib/main.dart
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text(
@@ -67,25 +67,45 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
+        backgroundColor: Colors.blue[50],
         body: TabBarView(
           children: [
             ListView.builder( 
               itemCount: _cardItems.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, i){
+                if (i.isOdd){
+                  return Divider();
+                }
+                final index = i~/2;
                 return IconButton(
                   icon: _cardItems[index].icon,
                   iconSize: _cardItems[index].iconSize,
                   color: _cardItems[index].color,
+                  splashColor: _cardItems[index].splashColor,
+                  padding: _cardItems[index].padding,
                   tooltip: _cardItems[index].tooltip,
                   onPressed: _cardItems[index].onPressed,
                 );
               },
-            ),// Display list of cards here
-            ListView( 
-              children: [
-
-              ],
-            ), // display list of calendars here
+            ),
+            ListView.builder( 
+              itemCount: _calendarItems.length,
+              itemBuilder: (context, i){
+                if (i.isOdd){
+                  return Divider();
+                }
+                final index = i~/2;
+                return new IconButton(
+                  icon: _calendarItems[index].icon,
+                  iconSize: _calendarItems[index].iconSize,
+                  color: _calendarItems[index].color,
+                  splashColor: _calendarItems[index].splashColor,
+                  padding: _calendarItems[index].padding,
+                  tooltip: _calendarItems[index].tooltip,
+                  onPressed: _calendarItems[index].onPressed,
+                );
+              },
+            ),
           ],
         ),
       ),
--- bluestone/lib/src/components/menuBuilder.dart
+++ bluestone/lib/src/components/menuBuilder.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 
 /// MenuBuilder is a class used to call the static buildItem method.
-/// see [buildItem] for more details.
+/// see [buildItem] for more details. 
 class MenuBuilder {
 
   static List<IconButton> buildItem(String type) {
@@ -13,8 +13,20 @@ class MenuBuilder {
       // add all of the calendars save for a user here.
     }
     else {
-      throw new Exception("Error: invalid type - Type being passed in is not of type String.");
+      throw new Exception("Error: invalid type - Type being passed in was unable to be determined.");
     }
+
+    var addIcon = new IconButton(
+      icon: Icon(Icons.add),
+      iconSize: 125.0,
+      color: Colors.blueAccent,
+      //padding: EdgeInsets.all(25.0),
+      splashColor: Colors.grey,
+      tooltip: "Tap me to create a new $type!",
+      onPressed: null,
+    );
+
+    list.add(addIcon);
     return list;
   }
 }
